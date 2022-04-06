119f4defe2c4ea2de2fe4d99fb1b4089d48d5db5
Hackerprod <hackerprodlive@gmail.com>
2022-04-06 01:21:29 -0400

    Settings implementation

---
+++ [SKYNET] Content Searcher/Manager/SettingsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKYNET
+{
+    public class SettingsManager
+    {
+    }
+    public class Settings
+    {
+        public string DirectoryPath { get; set; }
+        public string KeyToFind { get; set; }
+        public string Extention { get; set; }
+    }
+}
